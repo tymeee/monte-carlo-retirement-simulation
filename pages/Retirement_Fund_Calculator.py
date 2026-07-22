0cd154e634c0ad344f606916b37752f1a3d2285e
tymeee <tymeyeetboi@gmail.com>
2026-07-22 11:25:08 +0700

    Change minimum values for expense inputs to zero

--- pages/Retirement_Fund_Calculator.py
+++ pages/Retirement_Fund_Calculator.py
@@ -20,19 +20,19 @@ st.title("Retirement Funds Calculator")
 col1, col2, col3 = st.columns(3)
 st.write("ประเมินค่าใช้จ่ายแต่ละประเภทต่อเดือน")
 with col1:
-    household = st.number_input("ค่าใช้จ่ายเกี่ยวกับบ้าน", min_value = 1000)
-    bills = st.number_input ("ค่าสาธารณูปโภค (ค่าไฟฟ้า, ค่าน้ำประปา, อื่น ๆ)" , min_value = 1000)
-    leisure = st.number_input ("ค่าท่องเที่ยว/สันทนาการ", min_value = 1000)
-    other = st.number_input("ค่าใช้จ่ายอื่น ๆ", min_value = 1000)
+    household = st.number_input("ค่าใช้จ่ายเกี่ยวกับบ้าน", min_value = 0)
+    bills = st.number_input ("ค่าสาธารณูปโภค (ค่าไฟฟ้า, ค่าน้ำประปา, อื่น ๆ)" , min_value = 0)
+    leisure = st.number_input ("ค่าท่องเที่ยว/สันทนาการ", min_value = 0)
+    other = st.number_input("ค่าใช้จ่ายอื่น ๆ", min_value = 0)
 with col2:
-    Food_and_dining = st.number_input ("ค่าใช้จ่ายเกี่ยวกับอาหาร", min_value = 1000)
-    clothes = st.number_input ("ค่าเสื้อผ้า เครื่องแต่งกาย", min_value = 1000)
-    travel = st.number_input ("ค่าเดินทาง", min_value = 1000)
-    donate = st.number_input("ค่าบริจาค/ทำบูญ", min_value = 1000)
+    Food_and_dining = st.number_input ("ค่าใช้จ่ายเกี่ยวกับอาหาร", min_value = 0)
+    clothes = st.number_input ("ค่าเสื้อผ้า เครื่องแต่งกาย", min_value = 0)
+    travel = st.number_input ("ค่าเดินทาง", min_value = 0)
+    donate = st.number_input("ค่าบริจาค/ทำบูญ", min_value = 0)
 with col3:
-    hospital = st.number_input ("ค่าใช้จ่ายเกี่ยวกับสุขภาพ",min_value = 1000)
-    rent = st.number_input ("ค่าผ่อนบ้าน/ผ่อนรถ", min_value = 1000)
-    child = st.number_input("ค่าเลี้ยงลูกหลาน", min_value =1000)
+    hospital = st.number_input ("ค่าใช้จ่ายเกี่ยวกับสุขภาพ",min_value = 0)
+    rent = st.number_input ("ค่าผ่อนบ้าน/ผ่อนรถ", min_value = 0)
+    child = st.number_input("ค่าเลี้ยงลูกหลาน", min_value =0)
 
 inf_rate = st.slider(
     "Inflation Rate",

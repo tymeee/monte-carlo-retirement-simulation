4ae0b5f9bc4bca284e66e09bf16b723c48098e70
tymeee <tymeyeetboi@gmail.com>
2026-07-22 12:37:36 +0700

    Add keys to input fields in retirement calculator

--- pages/Retirement_Fund_Calculator.py
+++ pages/Retirement_Fund_Calculator.py
@@ -190,19 +190,19 @@ st.title("Retirement Funds Calculator")
 col1, col2, col3 = st.columns(3)
 st.write("ประเมินค่าใช้จ่ายแต่ละประเภทต่อเดือน")
 with col1:
-    household = thb_number_input(label = "ค่าใช้จ่ายเกี่ยวกับบ้าน", min_value = 0)
-    bills = thb_number_input (label = "ค่าสาธารณูปโภค (ค่าไฟฟ้า, ค่าน้ำประปา, อื่น ๆ)" , min_value = 0)
-    leisure = thb_number_input (label = "ค่าท่องเที่ยว/สันทนาการ", min_value = 0 )
-    other = thb_number_input(label = "ค่าใช้จ่ายอื่น ๆ", min_value = 0)
+    household = thb_number_input(label = "ค่าใช้จ่ายเกี่ยวกับบ้าน", min_value = 0, key = "household")
+    bills = thb_number_input (label = "ค่าสาธารณูปโภค (ค่าไฟฟ้า, ค่าน้ำประปา, อื่น ๆ)" , min_value = 0, key = "bills")
+    leisure = thb_number_input (label = "ค่าท่องเที่ยว/สันทนาการ", min_value = 0, key = "leisure")
+    other = thb_number_input(label = "ค่าใช้จ่ายอื่น ๆ", min_value = 0, key = "other")
 with col2:
-    Food_and_dining = thb_number_input (label = "ค่าใช้จ่ายเกี่ยวกับอาหาร", min_value = 0 )
-    clothes = thb_number_input (label = "ค่าเสื้อผ้า เครื่องแต่งกาย", min_value = 0 )
-    travel = thb_number_input (label = "ค่าเดินทาง", min_value = 0 )
-    donate = thb_number_input(label = "ค่าบริจาค/ทำบูญ", min_value = 0 )
+    Food_and_dining = thb_number_input (label = "ค่าใช้จ่ายเกี่ยวกับอาหาร", min_value = 0, key = "Food_and_dining" )
+    clothes = thb_number_input (label = "ค่าเสื้อผ้า เครื่องแต่งกาย", min_value = 0, key = "clothes" )
+    travel = thb_number_input (label = "ค่าเดินทาง", min_value = 0, key = "travel" )
+    donate = thb_number_input(label = "ค่าบริจาค/ทำบูญ", min_value = 0, key = "donate" )
 with col3:
-    hospital = thb_number_input (label = "ค่าใช้จ่ายเกี่ยวกับสุขภาพ",min_value = 0 )
-    rent = thb_number_input (label = "ค่าผ่อนบ้าน/ผ่อนรถ", min_value = 0)
-    child = thb_number_input(label = "ค่าเลี้ยงลูกหลาน", min_value =0 )
+    hospital = thb_number_input (label = "ค่าใช้จ่ายเกี่ยวกับสุขภาพ",min_value = 0, key = "hospital" )
+    rent = thb_number_input (label = "ค่าผ่อนบ้าน/ผ่อนรถ", min_value = 0, key = "rent")
+    child = thb_number_input(label = "ค่าเลี้ยงลูกหลาน", min_value =0, key = "child" )
 
 inf_rate = st.slider(
     "Inflation Rate",
